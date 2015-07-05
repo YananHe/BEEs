605053385b1e0963a30030c59b19f9e3d9ea410e
YananHe <972260358@qq.com>
2015-07-05 20:25:01 +0800

    Beginning my new application

---
+++ include/base/BeesApp.h
@@ -0,0 +1,22 @@
+#ifndef BEESAPP_H
+#define BEESAPP_H
+
+#include "MooseApp.h"
+
+class BeesApp;
+
+template<>
+InputParameters validParams<BeesApp>();
+
+class BeesApp : public MooseApp
+{
+public:
+  BeesApp(const std::string & name, InputParameters parameters);
+  virtual ~BeesApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* BEESAPP_H */
---
+++ src/base/BeesApp.C
@@ -0,0 +1,54 @@
+#include "BeesApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+
+template<>
+InputParameters validParams<BeesApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+
+  params.set<bool>("use_legacy_uo_initialization") = false;
+  params.set<bool>("use_legacy_uo_aux_computation") = false;
+  return params;
+}
+
+BeesApp::BeesApp(const std::string & name, InputParameters parameters) :
+    MooseApp(name, parameters)
+{
+  srand(processor_id());
+
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  BeesApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  BeesApp::associateSyntax(_syntax, _action_factory);
+}
+
+BeesApp::~BeesApp()
+{
+}
+
+// External entry point for dynamic application loading
+extern "C" void BeesApp__registerApps() { BeesApp::registerApps(); }
+void
+BeesApp::registerApps()
+{
+  registerApp(BeesApp);
+}
+
+// External entry point for dynamic object registration
+extern "C" void BeesApp__registerObjects(Factory & factory) { BeesApp::registerObjects(factory); }
+void
+BeesApp::registerObjects(Factory & factory)
+{
+}
+
+// External entry point for dynamic syntax association
+extern "C" void BeesApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { BeesApp::associateSyntax(syntax, action_factory); }
+void
+BeesApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+}
